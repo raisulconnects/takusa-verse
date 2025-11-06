8afd8a451cd14e51bb2c49213ab65b1bdc9cab4c
Raisul <raisulconnects@gmail.com>
2025-11-06 15:11:17 +0600

    Testing

--- src/app/public-feed/page.jsx
+++ src/app/public-feed/page.jsx
@@ -2,6 +2,7 @@ import connectDB from "../../../lib/connectDB";
 import { getAllPosts } from "../../../lib/getAllPosts";
 import Post from "../Components/Post";
 import PostBox from "../Components/PostBox";
+import User from "../../../models/User";
 
 export const dynamic = "force-dynamic";
 // export const revalidate = 10;
